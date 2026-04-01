1708398435a8a40a4c73d403fdf6a33f5512a630
DivyashreeR25 <divyashreer254@gmail.com>
2026-04-01 23:40:59 +0530

    fix deployment

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 
-const mongoose = require("mongoose");
-
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
 
     await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000, // faster fail
+      serverSelectionTimeoutMS: 5000, 
     });
 
     console.log("MongoDB Connected");
