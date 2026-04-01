503bf50e51da9e1f49488a4b2bd8b98d4523232a
DivyashreeR25 <divyashreer254@gmail.com>
2026-04-02 01:17:19 +0530

    swagger

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -22,6 +22,32 @@ const {
  *         description: List of users
  */
 // ADMIN ONLY
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Create a new user (Register)
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *                 example: user
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ */
 router.post("/", authorizeRoles("admin"), createUser);
 router.get("/", protect, authorizeRoles("admin"), getUsers);
 router.put("/:id", protect, authorizeRoles("admin"), updateUser);
