1bbb51df955c9115c77ac97fc1145062032e124d
DivyashreeR25 <divyashreer254@gmail.com>
2026-04-01 23:18:30 +0530

    fix deployment

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 app.get("/", (req, res) => {
   res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
